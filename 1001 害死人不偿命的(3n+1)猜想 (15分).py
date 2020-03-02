94cfa28669bde0dc9dd64c942369de8a478371c3
willcaty <2410205011@qq.com>
2020-03-02 23:06:30 +0800

    增加1001

---
+++ 1001 害死人不偿命的(3n+1)猜想 (15分).py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+
+n = int(input())  # 接收参数
+step_number = 0
+while n != 1:  # 直到n=1返回步数
+    if n % 2 == 0:  # 判断奇偶
+        n = n / 2
+    else:
+        n = (3 * n + 1) / 2
+    step_number += 1  # 步数+1
+print(step_number)
