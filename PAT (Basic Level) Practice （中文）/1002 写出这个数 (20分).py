01644b4956c537cb72f136979f3b537daf047298
wang_h <wang_h@startimes.com.cn>
2020-03-03 16:43:33 +0800

    增加1002

--- PAT (Basic Level) Practice （中文）/1002 写出这个数 (20分).py
+++ PAT (Basic Level) Practice （中文）/1002 写出这个数 (20分).py
@@ -4,8 +4,7 @@ pin = {0: 'ling', 1: 'yi', 2: 'er', 3: 'san',
        4: 'si', 5: 'wu', 6: 'liu', 7: 'qi',
        8: 'ba', 9: 'jiu'}
 
-output = [pin.get(int(i)) for i in str(sum(list(map(int, input()))))]
-print(' '.join(output))
+print(' '.join([pin.get(int(i)) for i in str(sum(list(map(int, input()))))]))
 
 """
 将接收到的参数转换成List[int]类型
