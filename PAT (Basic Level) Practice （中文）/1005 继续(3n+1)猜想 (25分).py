05a54715542b119a2ce9e56d60279ce17d3fd08e
wang_h <wang_h@startimes.com.cn>
2020-03-04 17:32:51 +0800

    优化写法

--- PAT (Basic Level) Practice （中文）/1005 继续(3n+1)猜想 (25分).py
+++ PAT (Basic Level) Practice （中文）/1005 继续(3n+1)猜想 (25分).py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*-
 
 n = int(input())
-input_list = list(map(int, input().split()))
-key = []  # 计算过的值
-
-
-def callatz(list_):
-    for n in list_:  # 循环输入进来的列表
-        if n not in key:  # 当前值没有计算过
-            while n != 1:
-                if n % 2 == 0:
-                    n = n / 2
-                else:
-                    n = (3 * n + 1) / 2
-                key.append(int(n))
-
-
-callatz(input_list)
-key_list = []
-for i in input_list:
-    if i not in key:
-        key_list.append(i)
-key_list.sort(reverse=True)
-output = ''
-for i in key_list:
-    output = output + str(i) + ' '
-print(output[0:-1])
+s = set()  # 所有遍历过的数字的集合
+sr = set()  # 关键数的列表
+for i in map(int, input().split()):
+    if i not in s:
+        sr.add(i)
+    while i not in s:
+        s.add(int(i))
+        i = i % 2 and (3 * i + 1) / 2 or i / 2
+    sr.discard(i)
+print(' '.join(map(str, sorted(sr, reverse=True))))
